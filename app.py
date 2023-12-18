5ce66cf6dad4294cab3f542d533fcd48eed7493e
ridwan9013 <ridwaniskandar36@gmail.com>
2023-12-18 14:01:15 +0700

    perubahan

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
-from flask import Flask,redirect,url_for,render_template,request
+import os
+from os.path import join, dirname
 from dotenv import load_dotenv
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 app=Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
+
+SECRET_KEY = "SPARTA"
+
+
+
+
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
         # Handle POST Request here
         return render_template('index.html')
     return render_template('index.html')
-@app.route('/login_admin')
+
+
+@app.route('/login_admin',methods=['GET','POST'])
 def login_admin():
     return render_template("login_admin.html")
 
-@app.route('/login_user')
+@app.route("/login_user",methods=['GET','POST'])
 def login_user():
-    return render_template("login_user.html")
+    msg = request.args.get("msg")
+    return render_template("login_user.html", msg=msg)
     
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
     return render_template("register.html")
 
@@ -24,7 +54,7 @@ def register():
 def heroes():
     return render_template("heroes.html")
 
-@app.route('/hero_story')
+@app.route('/hero_story',methods=['GET'])
 def hero_story():
     return render_template("hero_story.html")
 
@@ -67,11 +97,13 @@ def dashboard_heroes():
 @app.route('/dahboard_story')
 def dashboard_story():
     return render_template("dashboard_hero_story.html")
+@app.route("/home")
+def home1():
+    return render_template("home.html")
 
 
 
 
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run("0.0.0.0", port=5000, debug=True)
